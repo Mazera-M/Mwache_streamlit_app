3edd29558de41a7f8709250e7473726cf60ece37
Mariam Mazera <63015495+Mazera-M@users.noreply.github.com>
2026-08-03 08:31:33 +0300

    Update page title to 'Mwache Catchment Activities'

--- Home.py
+++ Home.py
@@ -15,7 +15,7 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("Mwache Catchment Activities")
 
 st.markdown("""
     This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
