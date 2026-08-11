e38746da568319eb5e35c129e808a790b604e09d
Mariam Mazera <mazeramariam@gmail.com>
2026-08-11 10:56:37 +0300

    change outline color

--- pages/1_🌍_Terraces_Map.py
+++ pages/1_🌍_Terraces_Map.py
@@ -12,7 +12,7 @@ st.sidebar.image(logo)
 
 st.subheader("Terraces Map")
 
-m = leafmap.Map(center=[40, -100], zoom=6)
+m = leafmap.Map(center=[40, -100], zoom=4)
 m.add_basemap("HYBRID")
 
 wruas_url = "https://raw.githubusercontent.com/Mazera-M/Mwache_streamlit_app/refs/heads/main/data/mwache_wruas.geojson"
@@ -23,7 +23,23 @@ m.add_geojson(wruas_url, layer_name="mwache_wruas")
 m.add_geojson(terraces6_url, layer_name="mwacheterraces_6")
 m.add_geojson(terraces14_url, layer_name="mwacheterraces_14")
 
+geojson_path = "data/mwache_wruas.geojson"
+
+# style function that sets outline to black
+style_function = lambda feature: {
+    "color": "black",      # outline color (stroke)
+    "weight": 2,           # stroke width
+    "opacity": 1.0,
+    "fillColor": "#ffffff",# optional fill
+    "fillOpacity": 0.1,
+}
+
+# leafmap add_geojson accepts a style (callable or dict)
+m.add_geojson(geojson_path, layer_name="WRUAs", style=style_function)
+
+# display in Streamlit
 m.to_streamlit(height=700)
+
 
 
 
