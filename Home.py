fb323f6f7c0a178cb62e859040234b34d99d9a37
Mariam Mazera <mazeramariam@gmail.com>
2026-08-18 15:19:06 +0300

    home adjs

--- Home.py
+++ Home.py
@@ -47,5 +47,5 @@ st.sidebar.image(logo)
 # Load data
 df = pd.read_csv("data/Mwache_Interventions.csv") 
 Intervention = st.multiselect(
-        "Choose interventions", list(df.index), ["Afforestation", "Terraces"]
+        "Choose interventions", df.columns.tolist(), ["Afforestation", "Terraces"]
 )       
