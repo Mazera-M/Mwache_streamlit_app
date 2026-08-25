e1ca9f856089b89622c6db8d817836639a339bb4
Mariam Mazera <mazeramariam@gmail.com>
2026-08-25 16:29:55 +0300

    highlight terraces

--- pages/1_🌍_Terraces_Map.py
+++ pages/1_🌍_Terraces_Map.py
@@ -14,15 +14,14 @@ st.sidebar.image(logo)
 
 st.subheader("Terraces Map")
 
-m = leafmap.Map(center=[40, -100], zoom=9)
-m.add_basemap("HYBRID")
-
 # Load data
 all_terraces_df = pd.read_csv("data/all_terraces.csv")
 
-st.subheader("Financial Year")
+st.markdown("**Financial Year**")
 st.dataframe(all_terraces_df[["financial_year"]], use_container_width=True)
 
+m = leafmap.Map(center=[40, -100], zoom=9)
+m.add_basemap("HYBRID")
 
 wruas_url = "https://raw.githubusercontent.com/Mazera-M/Mwache_streamlit_app/refs/heads/main/data/mwache_wruas.geojson"
 terraces6_url = "https://raw.githubusercontent.com/Mazera-M/Mwache_streamlit_app/refs/heads/main/data/mwacheterraces_6.geojson"
